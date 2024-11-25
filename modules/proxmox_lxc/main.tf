d0a76b80b81d176f24d72ced01e4e41d18227462
AobaIwaki <aobaiwaki@gmail.com>
2024-11-25 07:32:44 +0000

    少しupdateしたけどまだ動かない

--- modules/proxmox_lxc/main.tf
+++ modules/proxmox_lxc/main.tf
@@ -4,7 +4,10 @@ resource "proxmox_lxc" "basic" {
   password     = "lxc_secret"
   unprivileged = true
   ostemplate   = "local:vztmpl/ubuntu-24.10-standard_24.10-1_amd64.tar.zst"
-
+  
+  vmid = 224
+  swap = 512
+  start = true
   ssh_public_keys = <<-EOT
     ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAFENRdM4NwJfgRO/MqntSOLZxSy0fTLkj0etUL/CnakA7gNI9YuZUYQxtGVa80Oc+6bZjT38Nd6Eico5RoRVAdlzwEEfQq+iy3v/W8c0ElybiSQmPIw7Mc7KaEyUhMb1LgmVleZYsEizJV/89/lNGvPVjUXkcEZsliIMROuFepQASUiuQ== iwakiaoiyou@AobaMacBookAir.local
   EOT
@@ -18,6 +21,8 @@ resource "proxmox_lxc" "basic" {
   network {
     name   = "eth0"
     bridge = "vmbr0"
-    ip     = "dhcp"
+    ip     = "192.168.11.224/24"
+    gw    = "192.168.11.1"
+    firewall = true
   }
 }
