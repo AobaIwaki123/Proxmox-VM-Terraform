f0389e287f2164189ab261c815e5337239393c79
AobaIwaki <aobaiwaki@gmail.com>
2024-11-25 16:14:32 +0000

    LXCの雛形を作成

--- modules/proxmox_lxc/main.tf
+++ modules/proxmox_lxc/main.tf
@@ -3,28 +3,29 @@ resource "proxmox_lxc" "basic" {
       nesting = true
   }
   
-  target_node  = "pve02"
-  hostname     = "lxc-basic"
+  vmid = var.vmid
+  target_node  = var.target_node
+  hostname     = var.hostname
+  ssh_public_keys = var.public_key
+
+  ostemplate   = "local:vztmpl/${var.template}.tar.zst"
+
+  cores  = var.cores
+  memory = var.memory
+
   unprivileged = true
-  ostemplate   = "local:vztmpl/ubuntu-24.10-standard_24.10-1_amd64.tar.zst"
-  
-  vmid = 240
-  swap = 512
-  start = true
-  ssh_public_keys = <<-EOT
-    ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAFENRdM4NwJfgRO/MqntSOLZxSy0fTLkj0etUL/CnakA7gNI9YuZUYQxtGVa80Oc+6bZjT38Nd6Eico5RoRVAdlzwEEfQq+iy3v/W8c0ElybiSQmPIw7Mc7KaEyUhMb1LgmVleZYsEizJV/89/lNGvPVjUXkcEZsliIMROuFepQASUiuQ== iwakiaoiyou@AobaMacBookAir.local
-  EOT
+  start       = true
 
   // Terraform will crash without rootfs defined
   rootfs {
-    storage = "local-lvm"
-    size    = "8G"
+    storage = var.storage
+    size    = "${var.disk_size}G"
   }
 
     network {
         name = "eth0"
-        bridge = "vmbr0"
-        ip = "192.168.11.240/24"
-        ip6 = "dhcp"
+        bridge = var.bridge
+        ip = "${var.ip_address}/24"
+        gw = var.gateway
     }
 }
