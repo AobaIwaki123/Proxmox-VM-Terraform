eb6549d8c7a1fb400159c51bdd5b5743aee223ff
AobaIwaki <aobaiwaki@gmail.com>
2024-11-25 04:20:58 +0000

    軽微な変更を追加

--- modules/proxmox_lxc/main.tf
+++ modules/proxmox_lxc/main.tf
@@ -4,6 +4,17 @@ resource "proxmox_lxc" "basic" {
   password     = "lxc_secret"
   unprivileged = true
   ostemplate   = "local:vztmpl/ubuntu-24.10-standard_24.10-1_amd64.tar.zst"
+  ostype      = "ubuntu"
+
+  ssh_public_keys = <<-EOT
+    ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAFENRdM4NwJfgRO/MqntSOLZxSy0fTLkj0etUL/CnakA7gNI9YuZUYQxtGVa80Oc+6bZjT38Nd6Eico5RoRVAdlzwEEfQq+iy3v/W8c0ElybiSQmPIw7Mc7KaEyUhMb1LgmVleZYsEizJV/89/lNGvPVjUXkcEZsliIMROuFepQASUiuQ== iwakiaoiyou@AobaMacBookAir.local
+  EOT
+
+  cores = 1
+  memory = "512"
+  swap = "512"
+
+  vmid = 240
 
   // Terraform will crash without rootfs defined
   rootfs {
